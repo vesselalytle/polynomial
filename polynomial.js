7d75e0de56354180edc14dece41e6ea3fd0b1295
vesselalytle <vesselalytle@gmail.com>
2018-07-30 18:32:24 -0400

    Add files via upload

---
+++ polynomial.js
@@ -0,0 +1,7 @@
+/*Vessela Lytle, S# 547557, COP 2801
+polynomial.js evaluates the following polynomial expression and 
+displays the results: 3x^3-5x^2+6, for x=2.55*/
+
+
+var x = 2.55;
+console.log((3*(x**3))-(5*(x**2))+6);
